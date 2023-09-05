35cef7b0fa6511a3831a4d28f2d315b6fd173c2c
Parham Moieni <p.moieni.2006@gmail.com>
2023-09-05 18:23:52 +0330

    fix manifest

--- src/App.svelte
+++ src/App.svelte
@@ -10,6 +10,9 @@
         href="/apple-touch-icon.png" />
     <link
         rel="icon"
+        href="/favicon.ico" />
+    <link
+        rel="icon"
         type="image/png"
         sizes="32x32"
         href="/favicon-32x32.png" />
@@ -19,15 +22,9 @@
         sizes="16x16"
         href="/favicon-16x16.png" />
     <link
-        rel="manifest"
-        href="/site.webmanifest" />
-    <link
         rel="mask-icon"
-        href="/safari-pinned-tab.svg"
+        href="/mask-icon.png"
         color="#5bbad5" />
-    <meta
-        name="msapplication-TileColor"
-        content="#2d89ef" />
     <meta
         name="theme-color"
         content="#ffffff" />
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,45 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svelte(), VitePWA({ registerType: "autoUpdate" })],
+    plugins: [
+        svelte(),
+        VitePWA({
+            registerType: "autoUpdate",
+            includeAssets: [
+                "favicon.ico",
+                "apple-touch-icon.png",
+                "safari-pinned-tab.svg",
+            ],
+            manifest: {
+                name: "Zenodo",
+                short_name: "zenodo",
+                description: "focus timer",
+                theme_color: "#ffffff",
+                icons: [
+                    {
+                        src: "/android-chrome-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/android-chrome-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/android-chrome-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any",
+                    },
+                    {
+                        src: "android-chrome-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                ],
+            },
+        }),
+    ],
 });
