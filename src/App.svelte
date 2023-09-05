73674ea51a23b55a785d0ef938dfd004bcc60036
Parham Moieni <p.moieni.2006@gmail.com>
2023-09-05 17:54:10 +0330

    favicons

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,36 @@
     import { settingsState } from "./lib/store/settings";
 </script>
 
+<svelte:head>
+    <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png" />
+    <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png" />
+    <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png" />
+    <link
+        rel="manifest"
+        href="/site.webmanifest" />
+    <link
+        rel="mask-icon"
+        href="/safari-pinned-tab.svg"
+        color="#5bbad5" />
+    <meta
+        name="msapplication-TileColor"
+        content="#2d89ef" />
+    <meta
+        name="theme-color"
+        content="#ffffff" />
+</svelte:head>
+
 <main class="{$settingsState.darkTheme ? 'dark' : ''} w-screen h-screen">
     <Home />
 </main>
