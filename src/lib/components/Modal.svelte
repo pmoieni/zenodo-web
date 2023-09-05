6ba1e901b5be22374c8c84c864575000364b844d
Parham Moieni <p.moieni.2006@gmail.com>
2023-09-06 01:17:49 +0330

    small fixes

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -11,13 +11,13 @@
         on:click|self={close}
         class="w-full h-full border-none outline-none flex items-center justify-center bg-zinc-700 bg-opacity-30 absolute top-0 left-0">
         <div
-            class="w-full h-full md:w-auto md:h-auto flex flex-col items-center bg-zinc-200 dark:bg-zinc-800 md:rounded-md">
+            class="w-full h-full md:w-auto md:h-auto md:max-w-[80%] md:max-h-[80%] flex flex-col items-center bg-zinc-200 dark:bg-zinc-800 md:rounded-md overflow-hidden">
             <div class="w-full h-full overflow-auto">
                 <slot />
             </div>
             <button
                 on:click={close}
-                class="w-full h-16 bg-zinc-300 dark:bg-zinc-700 text-gray-700 dark:text-gray-300"
+                class="w-full h-16 min-h-16 border-none outline-none bg-zinc-300 dark:bg-zinc-700 text-gray-700 dark:text-gray-300"
                 >Close</button>
         </div>
     </button>
