8befb2037d57f9a9ae1371a52e79c24014b5a6fd
Parham Moieni <62774242+pmoieni@users.noreply.github.com>
2024-06-11 18:53:12 +0330

    fix

--- src/App.svelte
+++ src/App.svelte
@@ -1,62 +1,19 @@
-<script lang="ts">
-    import Home from "./lib/pages/Home.svelte";
-    import { settingsState } from "./lib/store/settings";
-</script>
+
 
 <svelte:head>
-    <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png" />
-    <link
-        rel="icon"
-        href="/favicon.ico" />
-    <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png" />
-    <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png" />
-    <link
-        rel="mask-icon"
-        href="/mask-icon.png"
-        color="#5bbad5" />
-    <meta
-        name="theme-color"
-        content="#ffffff" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" href="/favicon.ico" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="mask-icon" href="/mask-icon.png" color="#5bbad5" />
+    <meta name="theme-color" content="#ffffff" />
 </svelte:head>
 
-<main
-    data-theme={$settingsState.darkTheme ? "dark" : "light"}
-    class="w-screen h-screen">
-    <Home />
+<main data-theme="{$settingsState.darkTheme" ? "dark" : "light"} class="w-screen h-screen">
+    <Home></Home>
 </main>
 
-<style lang="postcss">
-    @font-face {
-        font-family: Inter;
-        src: url(./assets/fonts/Inter-Regular.ttf);
-    }
 
-    @font-face {
-        font-family: Inter;
-        src: url(./assets/fonts/Inter-Light.ttf);
-        font-weight: 300;
-    }
-
-    @font-face {
-        font-family: Inter;
-        src: url(./assets/fonts/Inter-Bold.ttf);
-        font-weight: bold;
-    }
-
-    :global(*) {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-</style>
+<script>import "./app.css";
+import Home from "./lib/pages/Home.svelte";
+import { settingsState } from "./lib/store/settings";</script><script>import "./app.css";</script><style></style>
